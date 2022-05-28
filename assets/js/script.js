f980b4b9e35af0d670ca2baa00a0407381134861
Bin O <bin.ostrowski@gmail.com>
2022-05-28 08:16:08 -0700

    create file structure and basic HTML

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+// when page loads: "Coding Quiz Challenge h1", 
+//"p text underneth", 
+//"start Quiz button";
+// the counter in top right corner
+// view high score link in top left corner
+
+//when click start quiz button
+//coundown starts from 75 seconds
+// question in h1 
+// list 4 buttons
+// :hover applied to all buttons to turn color - also removes returned answer
+// correct answers assign integer 
+
+
+// when click button:
+// present next question
+// create top border with returned value in h2 under answer buttons
+// if statement: 
+// clicked false button (3 out of 4)- return "wrong" underneath border
+// click correct - return "correct"
+
+// "All Done!" - h1
+//"Your final score is __."
+// add assigned integers from correct answers and display
+//"Enter your initials:" form input with place holder text "initials" "Submit" bottom
+// return initals in CAPS
+// top border with returned value in h2 under form input
+//when hover over button - return value dissapears
+
+//"High Scores" -h1
+// does not show header
+// list high scores with 1. 2. 3. 
+// display form input with "initals - score"
+// buttons: "Go back" "Clear high score"
+// go back returns to welcome page
+// clear high score- clears high scores.
