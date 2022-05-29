6d614e670db95e2fe73dc10cd09eede3f328d5d0
Bin O <bin.ostrowski@gmail.com>
2022-05-29 13:28:51 -0700

    add first question page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,8 @@
-console.log('test');
-
 
 // var btnAnswer = document.querySelector("#btn-answer");
 var questionPage = document.querySelector("#question-page");
-// var taskIdCounter = 0;
+var highScoreBtn = document.querySelector("#high-scores-btn")
+var taskIdCounter = 0;
 
 
 // when page loads: "Coding Quiz Challenge h1", 
@@ -11,70 +10,94 @@ var questionPage = document.querySelector("#question-page");
 //"start Quiz button";
 // the counter in top right corner
 // view high score link in top left corner
-var createWelcomePage = function (){ 
-    var newPage = document.createElement("main")
+var newPage = document.createElement("main")
     newPage.className = "page-content";
     newPage.id = "page-content";
     document.body.appendChild(newPage);
-    
+
+var createWelcomePage = function (){ 
     var welcomeContent = document.createElement("div");
     newPage.appendChild(welcomeContent);
     welcomeContent.className = "welcome-page";
     welcomeContent.id = "welcome-page";
-    var welcomePageTitle = welcomeContent.innerHTML ='<h1 id="title-question" class = "title-question">Coding Quiz Challenge</h1><p id="content-text" class = "content-text">Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!</p><div id="buttons" class = "buttons"><button id="start-btn" class = "start-btn">Start Quiz</button></div>';
-    welcomeContent.appendChile(welcomePageTitle);
-   
-  
-    console.log(newPage, welcomeContent);
-   
-}
+    var welcomePageTitle = welcomeContent.innerHTML ='<h1 id="title-question" class = "title-question">Coding Quiz Challenge</h1>'
+    + '<p id="content-text" class = "content-text">Try to answer the following code-related questions within the time limit.' 
+    + 'Keep in mind that incorrect answers will penalize your score/time by ten seconds!</p>'
+    + '<div id="buttons" class = "buttons"><button id="start-btn" class = "start-btn">Start Quiz</button></div>';
+    console.log(welcomePageTitle);
+};
 createWelcomePage();
 
-var startBtn = document.getElementById("start-btn");
-    console.log(startBtn);
-    startBtn.addEventListener("click", questionPage);
-    
-
-var questionPage = function () {
-    // createWelcomePage.replaceWith(questionPage); //how do i get a new screen 
-    // var newPage = document.createElement("main")
-    // newPage.className = "page-content";
-    // newPage.id = "page-content";
-    // document.body.appendChild(newPage);
-
-    // var questions = document.createElement("div");
-    // newPage.appendChild(questions);
-    // questions.className = "question-page";
-    // questions.id = "question-page";
-    // var questionTitle = welcomeContent.innerHTML ='<h1 id="title-question" class = "title-question">first question</h1>';
-
-   
-  
-    console.log(questions);
-    document.body.appendChild(questions);
-    
-};
-
-
 //when click start quiz button
-
+var startBtn = document.getElementById("start-btn");
 
 //coundown starts from 75 seconds
 
 // create object with:questions, list of 4 questions, and result  
+var questionPage1 = function () {
+    function clearcontent(elementID) {
+        document.getElementById(elementID).innerHTML = "";
+    }
+    startBtn = clearcontent('page-content');
 
+    var questionPage = document.createElement("div");
+        newPage.appendChild(questionPage);
+        questionPage.className = "question-page";
+        questionPage.id = "question-page";
 
-// list 4 buttons
+    var question1 = document.createElement ("h1");
+        question1.className = "title-question";
+        question1.id = "first-question";
+        question1.innerHTML = "First Question";
+        questionPage.appendChild(question1);
+       
+    var answerList = document.createElement("ol"); // why is this not listing with numbers?
+        answerList.className = "answer-list";
+        answerList.id = "answer-list";
+        questionPage.appendChild(answerList);
+    
+    var answerBtnOne = document.createElement("ListItemButton");
+        answerBtnOne.className="btn-answer";
+        answerBtnOne.id= //attachAttributeID
+        answerBtnOne.innerHTML = "answer1";
+        answerList.appendChild(answerBtnOne);
+        console.log(answerBtnOne);
+        
+        var answerBtnTwo = document.createElement("ListItemButton");
+        answerBtnTwo.className="btn-answer";
+        answerBtnTwo.id= //attachAttributeID
+        answerBtnTwo.innerHTML = "answer2";
+        answerList.appendChild(answerBtnTwo);
+        console.log(answerBtnTwo);
+      
+        var answerBtnThree = document.createElement("ListItemButton");
+        answerBtnThree.className="btn-answer";
+        answerBtnThree.id= //attachAttributeID
+        answerBtnThree.innerHTML = "answer3";
+        answerList.appendChild(answerBtnThree);
+        console.log(answerBtnThree);
+
+        var answerBtnFour = document.createElement("ListItemButton");
+        answerBtnFour.className="btn-answer";
+        answerBtnFour.id= //attachAttributeID
+        answerBtnFour.innerHTML = "answer4";
+        answerList.appendChild(answerBtnFour);
+        console.log(answerBtnFour);
+    
+};
+
+// list 4 buttons - assign id's to each button
 // :hover applied to all buttons to turn color - also removes returned answer
-// correct answers assign integer 
 
+// if statement: 
+// clicked false buttonId (3 out of 4)- return "wrong" underneath border
+// click correct buttonId - return "correct with points"
+// correct answers assign integer to buttonId
 
 // when click button:
 // present next question
 // create top border with returned value in h2 under answer buttons
-// if statement: 
-// clicked false button (3 out of 4)- return "wrong" underneath border
-// click correct - return "correct"
+
 
 // "All Done!" - h1
 //"Your final score is __."
@@ -92,3 +115,5 @@ var questionPage = function () {
 // go back returns to welcome page
 // clear high score- clears high scores.
 
+startBtn.addEventListener("click", questionPage1);
+// highScoreBtn.addEventListener("click", highScoresPage);
